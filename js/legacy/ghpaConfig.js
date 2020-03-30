a129633df8155c14ec4408da34d061b3e64ebb16
scheidelg <scheidelg@gmail.com>
2020-03-30 17:22:07 -0400

    Delete ghpaConfig.js

--- js/legacy/ghpaConfig.js
+++
@@ -1,7 +0,0 @@
-const ghpaOrg = 'sans-blue-team';
-const ghpaRepo = 'cdnw2';
-const ghpaBranch = 'test';
-const ghpaDefaultHTMLfile = 'index.html';
-let ghpaSSOFlag = true;
-let ghpaFilename = '';
-let ghpaAuthOnlyFlag = false;
