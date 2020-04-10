563e8f81b870d3396827f8fcb6e544b8c7522415
scheidelg <scheidelg@gmail.com>
2020-04-10 17:53:26 -0400

    Update GHPA2.js

--- js/GHPA2.js
+++ js/GHPA2.js
@@ -1,4 +1,16 @@
 function ghpaReadConfig(configFile) {
+    const jsonData = fetch(configFile)
+    .then(function (response) {
+        if (!response.ok) {
+            throw new Error(`${response.status} HTTP response retrieving configuration file ${configFile}`);
+        }
+        return response.json();
+    })
+    .catch(function (errObject) {
+        console.error(errObject.message);
+    });
+    
+    let x=1;
 }
 
 ghpaReadConfig('/examples/ghpaConfig.json');
