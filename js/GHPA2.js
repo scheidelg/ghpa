0c3c5035ad4bcb0ac1d9a5eef112ea946df2ea8f
scheidelg <scheidelg@gmail.com>
2020-04-13 17:31:18 -0400

    Update GHPA2.js

--- js/GHPA2.js
+++ js/GHPA2.js
@@ -1,13 +1,13 @@
 'use strict';
 
 function recurseMe(configObject, parentString) {
-    if (! parentString) { parentString = '/'; }
+    if (! parentString) { parentString = '/'; }     // gotta start somewhere, boyo
     
-    for (let propertyName in configObject) {
-        if (configObject.hasOwnProperty(propertyName)) {
+    for (let propertyName in configObject) {        // iterate through all properties in the passed object
+        if (configObject.hasOwnProperty(propertyName)) {        // only continue if this is a non-inherited property
 console.log(parentString + propertyName);
-            if (typeof configObject[propertyName] == 'object') {
-                recurseMe(configObject[propertyName], parentString + propertyName + '/');
+            if (typeof configObject[propertyName] == 'object') {    // only recurse if this property is an object
+                recurseMe(configObject[propertyName], parentString + propertyName + '/');   // recurse into sub-properties, adding this property name to the parent string
             }
         }
     }
@@ -50,3 +50,55 @@ if (document.addEventListener) {
 }
 
 let ghpaConfig;
+
+// loginFormFile - any of:
+//  - alphanumeric
+//  - $-_.+!*'()\
+//  - %xx where xx is 20 through FF
+//
+// (keyformat)
+//  - any of:
+//     - alphanumeric
+//     - _-
+//  - cannot have sequential -_
+//  - cannot end with -_
+//
+// organization
+//  - may only contain alphanumeric characters or single hyphens, and cannot begin or end with a hyphen
+//
+// repository
+//  - may only contain alphanumeric, hyphen, underscore, period
+//
+// branch
+//  - cannot contain ^[\:?
+//
+// defaultHTMLfile
+//  - any of:
+//     - alphanumeric
+//     - $-_.+!*'()\
+//     - %xx where xx is 20 through FF
+//  - cannot start with a /
+const ghpaConfigSchema =
+{
+    "tokensOnly": "boolean",
+
+    "pageOptions": {
+        "authOnly": "boolean",
+        "SSO": "boolean"
+    },
+
+    "loginFormOptions": {
+        "loginFormFile": "/^(([0-9a-z\$\-_\.\+\!\*\'\(\),\/])|(%[2-9a-f][0-9a-f]))+$/i",
+    },
+
+    "ghpaClasses": {
+        "(keys)": {
+            "(keyformat)": "/^[a-z]([0-9a-z]|([\-_](?![\-_])))*(?<![\-_])$/i",
+            "organization": "/^[0-9a-z]([0-9a-z]|\-(?!\-))*(?<!\-)$/i",
+            "repository": "/^0-9a-z_.\-]/i",
+            "branch": "/^[^\^\[\\:\?]+$/",
+            "defaultHTMLfile": ""/^(([0-9a-z\$\-_\.\+\!\*\'\(\),])|(%[2-9a-f][0-9a-f]))+$/i"",
+            "onlyGetBody": "boolean"
+        }
+    }
+};
