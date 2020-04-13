359d95a639b2e9987b2f4bd18d920e6e3fb8dc70
scheidelg <scheidelg@gmail.com>
2020-04-13 12:47:01 -0400

    Update GHPA2.js

--- js/GHPA2.js
+++ js/GHPA2.js
@@ -1,7 +1,7 @@
 'use strict';
 
 async function ghpaReadConfig(configFile) {
-    const jsonData = await fetch(configFile)
+    return(await fetch(configFile)
     .then(function (response) {
         if (response.status != 200) {
             throw new Error(`${response.status} HTTP response retrieving configuration file ${configFile}`);
@@ -11,13 +11,22 @@ async function ghpaReadConfig(configFile) {
     .catch(function (errObject) {
         console.error(errObject.message);
 // return an empty JSON object here --- ?  test to see whether it's really necessary (or if not strictly necessary, desirable)
-    });
-    
-    let x=1;
+    }));
 }
 
 function ghpaInit() {
-    ghpaReadConfig('/examples/ghpaConfig.json');
+    // read the GHPA configuration file
+    ghpaConfig = ghpaReadConfig('/examples/ghpaConfig.json');
+
+    let x=1;
 }
 
-document.addEventListener('DOMContentLoaded', ghpaInit);
+if (document.addEventListener) {
+    // for modern browsers - run init after DOM is loaded
+    document.addEventListener('DOMContentLoaded', ghpaInit);
+} else {
+    // for older browsers - run init the window is fully loaded
+    window.onload=ghpaInit;
+}
+
+let ghpaConfig;
