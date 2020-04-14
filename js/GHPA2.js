70c04507d46941355ef5af9c428d05a80ac681df
scheidelg <scheidelg@gmail.com>
2020-04-14 18:07:41 -0400

    Update GHPA2.js

--- js/GHPA2.js
+++ js/GHPA2.js
@@ -14,7 +14,7 @@ function ghpaConfigPropertyCheck(propertyName, parentSchemaObject) {
         for (let keyformatPropertyName in parentSchemaObject['(keyformats)']) {
             if (parentSchemaObject['(keyformats)'].hasOwnProperty(keyformatPropertyName)) {        // only continue if this is a non-inherited property
 // so far just checking to see if there's *any* "(keyformat:*)", not necessarily a matching one
-                if (parentSchemaObject['(key:' + keyformatPropertyName] + ')') {        // after testing, switch to: `(key:{$keyformatPropertyName})`
+                if (parentSchemaObject['(key:' + keyformatPropertyName + ')']) {        // after testing, switch to: `(key:{$keyformatPropertyName})`
                     propertyMatch = keyformatPropertyName;
                 }
                     
