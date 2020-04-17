8f542faa3547eedf0a00bc48c7fa7d5883a7e9aa
scheidelg <scheidelg@gmail.com>
2020-04-17 10:35:41 -0400

    Update GHPA2.js

--- js/GHPA2.js
+++ js/GHPA2.js
@@ -162,7 +162,7 @@ function ghpaConfigSchemaLintCheck(configSchemaObject, parentString, configSchem
     let propertyString;
     let returnValue = true;
     
-    if (true) returnValue = true;
+    if (true) let asdfxyzzy = true;
     
     parentString = (! parentString) ? '/' : (parentString + ' /');
     if (! configSchemaRoot) { configSchemaRoot = configSchemaObject; }
